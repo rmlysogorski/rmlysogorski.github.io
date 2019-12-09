80ebd8328ce16c991806615512ce2a24ad39297f
Rich Lysogorski <rmlysogorski@gmail.com>
2019-12-09 12:13:28 -0500

    Removed Resume from site

--- js/animations.js
+++ js/animations.js
@@ -31,7 +31,7 @@ window.addEventListener("load", () => {
 function animatePage(){
     var p = document.getElementById('page');
     p.classList.add('animate_out');    
-    setTime(function(){p.classList.add('hide')}, 900);
+    setTime(function(){p.classList.add('hide')}, 800);
 }
 
 function delay (URL) {
